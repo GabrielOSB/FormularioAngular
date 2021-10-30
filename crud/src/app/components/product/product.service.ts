df2a3cb9e1230975d166e7c8643d0387fa732eed
gabriel <gabrielosb4@gmail.com>
2021-10-29 22:01:51 -0300

    Acessando API via APIKey

--- crud/src/app/components/product/product.service.ts
+++ crud/src/app/components/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Product } from "./product.model";
 import { Observable } from "rxjs";
 
@@ -11,22 +11,25 @@ import { Observable } from "rxjs";
 export class ProductService {
   baseUrl = "http://localhost:3001/products";
   baseUrlAPI = "http://localhost:55863/Product/v1/api";
+  private headers: HttpHeaders;
 
-  constructor(private snackbar: MatSnackBar, private http: HttpClient) {}
+  constructor(private snackbar: MatSnackBar, private http: HttpClient) {
+    this.headers = new HttpHeaders({ APIKEY: "ACBY5j8EvKkmTeUCup6AOAm2" });
+  }
 
   showMessage(msg: string, isError: boolean = false): void {
     this.snackbar.open(msg, "X", {
       duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top",
-      panelClass: isError ? ['msg-error'] : ['msg-success']
+      panelClass: isError ? ["msg-error"] : ["msg-success"],
     });
   }
 
   getProductAPI() {
-    return this.http.get(this.baseUrlAPI)
+    return this.http.get(this.baseUrlAPI, { headers: this.headers });
   }
-  
+
   create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product);
   }
