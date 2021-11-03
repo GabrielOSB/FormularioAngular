8da282f0444b58ee0cb1567328ec2f00124523f2
gabriel <gabrielosb4@gmail.com>
2021-11-02 22:50:10 -0300

    Fazendo update via banco

--- crud/src/app/components/product/product-read/product-read.component.ts
+++ crud/src/app/components/product/product-read/product-read.component.ts
@@ -1,27 +1,23 @@
-import { Product } from './../product.model';
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
+import { Product } from "./../product.model";
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-read',
-  templateUrl: './product-read.component.html',
-  styleUrls: ['./product-read.component.css']
+  selector: "app-product-read",
+  templateUrl: "./product-read.component.html",
+  styleUrls: ["./product-read.component.css"],
 })
 export class ProductReadComponent implements OnInit {
-
-  products!: Product[]
+  products!: Product[];
   productsAPI!: any[];
-  displayedColumns = ['name', 'price', 'action']
+  displayedColumns = ["name", "price", "action"];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProductAPI().subscribe((data: any) => {
-      debugger;
       this.productsAPI = data.products;
-      // this.products = result;
-      console.log(data)
-    })
+      console.log(data);
+    });
   }
-
 }
--- crud/src/app/components/product/product-update/product-update.component.ts
+++ crud/src/app/components/product/product-update/product-update.component.ts
@@ -11,39 +11,42 @@ import { Product } from "./../product.model";
   styleUrls: ["./product-update.component.css"],
 })
 export class ProductUpdateComponent implements OnInit {
-  product!: Product;
   productsAPI!: any;
+
+  product: Product = {
+    id: 0,
+    title: "",
+    price: 0,
+  };
 
   constructor(
     private ProductService: ProductService,
     private router: Router,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get("id");
-    debugger;
-    this.ProductService.readById(id!).subscribe((product : any) => {
-      debugger;
+    this.ProductService.readById(id!).subscribe((product: any) => {
       this.productsAPI = product;
     });
   }
 
   updateProduct(title: string, price: number, id: number): void {
     debugger;
+
     this.product.id = id;
     this.product.title = title;
-    this.product.price = price;
+    this.product.price = Number(price);
 
     this.ProductService.update(this.product).subscribe((data: any) => {
-      this.ProductService.showMessage('Product Created !')
+      this.ProductService.showMessage("Produto alterado !");
 
-      this.router.navigate(['/products'])
-    })
+      this.router.navigate(["/products"]);
+    });
   }
 
   cancel(): void {
     this.router.navigate(["/products"]);
   }
 }
-
